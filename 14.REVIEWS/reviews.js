ae8a2127892e2aac83e56c976b578bdcb65e307e
Sadariun <zyxeliard@gmail.com>
2025-10-16 13:48:06 +0300

    reviews fix?

--- 14.REVIEWS/reviews.js
+++ 14.REVIEWS/reviews.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   if (Swiper) {
-    const swiperReviews = new Swiper("#reviews .swiper", {
+    const swiperreviews_2 = new Swiper("#reviews_2 .swiper", {
       loop: true,
       slidesPerView: "auto",
       spaceBetween: 20,
