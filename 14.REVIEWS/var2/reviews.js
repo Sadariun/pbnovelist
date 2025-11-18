95265717314451f8c91615a98d859704e0e8ed50
Takoyakor <zyxeliard@gmail.com>
2025-11-18 15:25:41 +0300

    fixed sections + new variants

--- 14.REVIEWS/var2/reviews.js
+++ 14.REVIEWS/var2/reviews.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   if (Swiper) {
-    const swiperreviews_2 = new Swiper("#reviews_2 .swiper", {
+    const swiperrecensioni = new Swiper("#recensioni .swiper", {
       loop: true,
       slidesPerView: "auto",
       spaceBetween: 20,
+      navigation: {
+                nextEl: ".reviewControlsRight",
+                prevEl: ".reviewControlsLeft",
+                disabledClass: "disabled",
+            },
       breakpoints:{
         769: {
           slidesPerView: 1
         },
         1024:{
-          slidesPerView: 2
+          slidesPerView: 1.3
         }
       }
     });
